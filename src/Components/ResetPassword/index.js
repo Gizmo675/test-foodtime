7017a2bc20d2904fee640e70cf418e8ff4d22625
Romain <septier.romain@gmail.com>
2020-10-07 10:25:20 +0200

    adding toast on reset password

--- src/Components/ResetPassword/index.js
+++ src/Components/ResetPassword/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Input, Form, Image, Grid, Divider} from 'semantic-ui-react'
+import {toast} from 'react-toastify'
 import {Link} from 'react-router-dom'
+
+// styles
+import 'react-toastify/dist/ReactToastify.css';
+import './resetPassword.css'
 
 import Foodtime from '../../assets/img/foodtime-mockup.jpg'
 
+toast.configure()
+
 function ResetPassword() {
+
+  const [email, setEmail] = useState('')
+
+  const handleSubmit=()=>{
+    if(email !== ''){
+      toast.success('Un mail vous a été envoyé afin de re-initialiser votre mot de passe')
+    }
+  }
+
   return(
     <Grid textAlign='center' verticalAlign='middle' >
     <Grid.Column width={6}>
@@ -18,10 +34,20 @@ function ResetPassword() {
         <p>Aucun problème, ce sont des choses qui arrivent. Remplissez votre adresse mail afin de recevoir un lien de ré-initialisation</p>
         <Form>
           <Form.Field>
-            <Input placeholder='Email' type='email' />
+            <Input
+              placeholder='Email'
+              type='email'
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Field>
           <Form.Field>
-            <Button type='submit' primary>Ré-initialiser</Button>
+            <Button 
+              type='submit'
+              primary
+              onClick={()=>handleSubmit()}
+             >Ré-initialiser</Button>
           </Form.Field>
         </Form>
         <Divider />
